cd123c521ec0a93d23534d65bd249550711f955c
ThomasTojo <thomas.tojo@swarco.com>
2017-03-03 09:58:11 +0000

    Created freertos blinky
    demo project

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+This is a LPCXpresso demo project
+
+freertos blinky project
---
+++ Release/freertos/src/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../freertos/src/FreeRTOSCommonHooks.c \
+../freertos/src/croutine.c \
+../freertos/src/event_groups.c \
+../freertos/src/heap_3.c \
+../freertos/src/list.c \
+../freertos/src/port.c \
+../freertos/src/queue.c \
+../freertos/src/tasks.c \
+../freertos/src/timers.c 
+
+OBJS += \
+./freertos/src/FreeRTOSCommonHooks.o \
+./freertos/src/croutine.o \
+./freertos/src/event_groups.o \
+./freertos/src/heap_3.o \
+./freertos/src/list.o \
+./freertos/src/port.o \
+./freertos/src/queue.o \
+./freertos/src/tasks.o \
+./freertos/src/timers.o 
+
+C_DEPS += \
+./freertos/src/FreeRTOSCommonHooks.d \
+./freertos/src/croutine.d \
+./freertos/src/event_groups.d \
+./freertos/src/heap_3.d \
+./freertos/src/list.d \
+./freertos/src/port.d \
+./freertos/src/queue.d \
+./freertos/src/tasks.d \
+./freertos/src/timers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+freertos/src/%.o: ../freertos/src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DNDEBUG -D__CODE_RED -D__USE_LPCOPEN -DCORE_M3 -I"C:\Users\Thomas\Documents\LPCXpresso_8.2.2_650\workspace\lpc_chip_18xx\inc" -I"C:\Users\Thomas\Documents\LPCXpresso_8.2.2_650\workspace\lpc_board_keil_mcb_1857\inc" -I"C:\Users\Thomas\Documents\LPCXpresso_8.2.2_650\workspace\freertos_blinky\example\inc" -I"C:\Users\Thomas\Documents\LPCXpresso_8.2.2_650\workspace\freertos_blinky\freertos\inc" -Os -g -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
